70c8bddd63cb4ec027a151b77e46083d36b18bf6
PauloBrand7 <paulo.brand77@gmail.com>
2022-08-06 13:37:18 -0300

    Commit Inicial

---
+++ app/src/main/java/br/com/minhareceita/MainActivity.kt
@@ -0,0 +1,18 @@
+package br.com.minhareceita
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.minhareceita.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/br/com/minhareceita/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package br.com.minhareceita.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
