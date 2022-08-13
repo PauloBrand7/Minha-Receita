657762e3e15d7b057be5886793d75607b1f0dcd9
PauloBrand7 <paulo.brand77@gmail.com>
2022-08-13 17:04:40 -0300

    Cria models da camada presentation

---
+++ app/src/main/java/br/com/minhareceita/presentation/model/FoodCategoriesUI.kt
@@ -0,0 +1,5 @@
+package br.com.minhareceita.presentation.model
+
+data class FoodCategoriesUI(
+    val foodCategory: List<FoodCategoryUI>
+)
---
+++ app/src/main/java/br/com/minhareceita/presentation/model/FoodCategoryUI.kt
@@ -0,0 +1,8 @@
+package br.com.minhareceita.presentation.model
+
+data class FoodCategoryUI(
+    val categoryId: String,
+    val categoryName: String,
+    val categoryDescription: String,
+    val categoryImage: String,
+)
